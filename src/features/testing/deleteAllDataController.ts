3ba7bae15660d3735d1f3bded55b1d96770ac516
Gumay Vierdiieva <Humai.Vierdiieva@IRIS.CO.UK>
2024-07-04 01:29:35 +0100

    Clean up DB

--- src/features/testing/deleteAllDataController.ts
+++ src/features/testing/deleteAllDataController.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from 'express'
 import { setDB } from '../../db/db'
+import { blogsCollection, postsCollection } from '../../db/mongo-db';
 
-export const deleteAllDataController = (req: Request, res: Response) => {
-    setDB()
+export const deleteAllDataController = async (req: Request, res: Response): Promise<void> => {
+  try {
+       await blogsCollection.deleteMany({});
+       await postsCollection.deleteMany({});
+
+      } catch (error) {
+        console.error('Error deleting blog data:', error);
+      }
     res
         .sendStatus(204)
 }
