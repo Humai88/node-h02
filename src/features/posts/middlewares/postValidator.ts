a29715c019b89e0e87bc16a25bf1a518e2eaa4ef
Gumay Vierdiieva <Humai.Vierdiieva@IRIS.CO.UK>
2024-07-22 00:24:20 +0100

    Fix validation

--- src/features/posts/middlewares/postValidator.ts
+++ src/features/posts/middlewares/postValidator.ts
@@ -1,7 +1,6 @@
 import { body, param, query, ValidationChain } from 'express-validator';
 import { inputErrors } from '../../../global/middlewares/inputErrors';
 import { adminMiddleware } from '../../../global/middlewares/adminMiddleware';
-import { blogsCollection } from '../../../db/mongo-db';
 import { SortDirection } from '../../../models/QueryModel';
 
 export const postValidator = [
@@ -9,13 +8,7 @@ export const postValidator = [
   body('title').isString().withMessage('Title must be a string').trim().isLength({ min: 1, max: 30 }).withMessage('Title must be between 1 and 30 characters'),
   body('content').isString().withMessage('Content must be a string').trim().isLength({ min: 1, max: 1000 }).withMessage('Content must be between 1 and 1000 characters'),
   body('shortDescription').isString().withMessage('Short description must be a string').trim().isLength({ min: 1, max: 100 }).withMessage('Short description must be between 1 and 100 characters'),
-  body('blogId').isString().withMessage('Blog ID must be a string').trim().isLength({ min: 1 }).withMessage('Blog ID is required').custom(async value => {
-    const blog = await blogsCollection.findOne({ _id: value })
-    if (!blog) {
-      throw new Error('There is no such blog');
-    }
-    return true
-  }), 
+  body('blogId').isString().withMessage('Blog ID must be a string').trim().isLength({ min: 1 }).withMessage('Blog ID is required'), 
   inputErrors
 ];
 
