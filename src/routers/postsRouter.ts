2391f3d356be3e6bfd6465ae80fc2c85e59cb72f
Gumay Vierdiieva <Humai.Vierdiieva@IRIS.CO.UK>
2024-08-25 16:08:36 +0100

    Add validation to objectId

--- src/routers/postsRouter.ts
+++ src/routers/postsRouter.ts
@@ -4,7 +4,7 @@ import { createPostController } from '../features/posts/controllers/createPostCo
 import { findPostController } from '../features/posts/controllers/findPostController'
 import { deletePostController } from '../features/posts/controllers/deletePostController'
 import { updatePostController } from '../features/posts/controllers/updatePostController'
-import { postIdParamValidator, postQueryValidator, postValidator } from '../features/posts/middlewares/postValidator'
+import { postIdParamValidator, postQueryValidator, postValidator, validateObjectId } from '../features/posts/middlewares/postValidator'
 import { adminMiddleware } from '../global/middlewares/adminMiddleware'
 import { applyPostQueryDefaults } from '../features/posts/middlewares/postDefaultQueryValues'
 import { createCommentForPostController } from '../features/posts/controllers/createCommentForPostController'
@@ -19,5 +19,5 @@ postsRouter.get('/:id', findPostController)
 postsRouter.delete('/:id', adminMiddleware, deletePostController)
 postsRouter.put('/:id', ...postValidator, updatePostController)
 postsRouter.post('/:postId/comments', ...commentValidator, createCommentForPostController)
-postsRouter.get('/:postId/comments', applyPostQueryDefaults,  postIdParamValidator, getCommentsForPostController)
+postsRouter.get('/:postId/comments', applyPostQueryDefaults, postIdParamValidator, getCommentsForPostController)
 
