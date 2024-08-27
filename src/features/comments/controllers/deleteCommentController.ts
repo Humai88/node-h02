22a1863e2c647fc3674d416de6daee454b2703bb
Gumay Vierdiieva <Humai.Vierdiieva@IRIS.CO.UK>
2024-08-27 16:07:49 +0100

    Revert changes

--- src/features/comments/controllers/deleteCommentController.ts
+++ src/features/comments/controllers/deleteCommentController.ts
@@ -4,15 +4,15 @@ import { commentsService } from '../../../domains/comments-service';
 import { commentsDBRepository } from '../../../repositories/commentsDBRepository';
 
 
-export const deleteCommentController = async (req: Request<{ id: string }>, res: Response<null | ErrorResultModel>) => {
+export const deleteCommentController = async (req: Request<{ commentId: string }>, res: Response<null | ErrorResultModel>) => {
     try {
-        const { id } = req.params
-        const commentToDelete = await commentsService.deleteComment(id)
+        const { commentId } = req.params
+        const commentToDelete = await commentsService.deleteComment(commentId)
         if (!commentToDelete) {
             res.status(404).json({ errorsMessages: [{ message: 'Comment not found', field: 'commentId' }] })
             return
         }
-        const isUserAuthorOfComment = await commentsDBRepository.checkIfUserIfAuthorOfComment(req.user!, id)
+        const isUserAuthorOfComment = await commentsDBRepository.checkIfUserIfAuthorOfComment(req.user!, commentId)
         if (!isUserAuthorOfComment) {
             res.status(403).json({ errorsMessages: [{ message: 'You are not the author of this comment', field: 'userId' }] })
             return
