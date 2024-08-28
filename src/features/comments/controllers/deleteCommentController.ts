853133300a28ea5a5509d738ab54bdc263ca1c60
Gumay Vierdiieva <Humai.Vierdiieva@IRIS.CO.UK>
2024-08-28 11:33:32 +0100

    fIX VALIDATION ORDER FOR DELETE COMMENT CONTROLLER

--- src/features/comments/controllers/deleteCommentController.ts
+++ src/features/comments/controllers/deleteCommentController.ts
@@ -7,19 +7,21 @@ import { commentsDBRepository } from '../../../repositories/commentsDBRepository
 export const deleteCommentController = async (req: Request<{ commentId: string }>, res: Response<null | ErrorResultModel>) => {
     try {
         const { commentId } = req.params
-        const commentToDelete = await commentsService.deleteComment(commentId)
-        if (!commentToDelete) {
-            res.status(404).json({ errorsMessages: [{ message: 'Comment not found', field: 'commentId' }] })
-            return
-        }
         const isUserAuthorOfComment = await commentsDBRepository.checkIfUserIfAuthorOfComment(req.user!, commentId)
         if (!isUserAuthorOfComment) {
             res.status(403).json({ errorsMessages: [{ message: 'You are not the author of this comment', field: 'userId' }] })
             return
+        } else {
+            const commentToDelete = await commentsService.deleteComment(commentId)
+            if (!commentToDelete) {
+                res.status(404).json({ errorsMessages: [{ message: 'Comment not found', field: 'commentId' }] })
+                return
+            }
+         
+            return res
+                .sendStatus(204)
         }
-        return res
-            .sendStatus(204)
-
+      
     } catch (error) {
         return res.status(500).json({
             errorsMessages: [{ message: 'Internal server error', field: 'server' }]
