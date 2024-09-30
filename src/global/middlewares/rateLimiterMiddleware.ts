d8c76ca4b56a6b20c24d7a7d44dfbb34f3957eca
Gumay Vierdiieva <Humai.Vierdiieva@IRIS.CO.UK>
2024-09-30 23:57:19 +0100

    Add next call

--- src/global/middlewares/rateLimiterMiddleware.ts
+++ src/global/middlewares/rateLimiterMiddleware.ts
@@ -1,5 +1,5 @@
 import rateLimit from 'express-rate-limit';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 export const rateLimiter = rateLimit({
   windowMs: 10 * 1000, 
@@ -9,11 +9,13 @@ export const rateLimiter = rateLimit({
   legacyHeaders: false,
   
 
-  handler: (req: Request, res: Response) => {
+  handler: (req: Request, res: Response, next: NextFunction) => {
     res.status(429).json({
       error: 'Too many requests, please try again later.',
       retryAfter: Math.ceil(req.rateLimit.resetTime.getTime() - Date.now()) / 1000
     });
+    next()
   },
   
+
 });
