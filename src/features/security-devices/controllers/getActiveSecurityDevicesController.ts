fb8f7fb301002854cafbf72d2af617d6f8a390f8
Gumay Vierdiieva <Humai.Vierdiieva@IRIS.CO.UK>
2024-10-06 11:16:38 +0100

    Return onlty sessions for a user

--- src/features/security-devices/controllers/getActiveSecurityDevicesController.ts
+++ src/features/security-devices/controllers/getActiveSecurityDevicesController.ts
@@ -2,10 +2,14 @@ import { Request, Response } from 'express';
 import { ErrorResultModel } from '../../../models/ErrorResultModel';
 import {DeviceViewModel} from '../../../models/DeviceModel'
 import {deviceSessionsQueryRepository} from '../../../repositories/deviceSessionsQueryRepository'
+import { jwtService } from '../../../application/jwtService';
 
 export const getActiveSecurityDevicesController = async (req: Request<any>, res: Response<DeviceViewModel[] | ErrorResultModel>) => {
     try {
-        const sessions = await deviceSessionsQueryRepository.getSessions()
+        const refreshToken = req.cookies.refreshToken;
+        const verificationResult = await jwtService.verifyRefreshToken(refreshToken);
+        const { payload } = verificationResult;
+        const sessions = await deviceSessionsQueryRepository.getSessions(payload!.userId)
         return res
           .status(200)
           .json(sessions)
